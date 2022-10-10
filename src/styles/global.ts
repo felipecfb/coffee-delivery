ff7961a83c7cfd010752baab2bfb08ea01100e76
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-10-10 09:40:44 -0300

    💄 feat: creating theme and global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.purple.default};
+  }
+  
+  body {
+    background: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.text};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+export const defaultTheme = {
+  white: '#FFFFFF',
+  background: '#FAFAFA',
+  card: '#F3F2F2',
+  input: '#EDEDED',
+  button: '#E6E5E5',
+  hover: '#D7D5D5',
+  label: '#8D8686',
+  text: '#574F4D',
+  subtitle: '#403937',
+  title: '#272221',
+
+  purple: {
+    default: '#8047F8',
+    dark: '#4B2995',
+    light: '#EBE5F9',
+  },
+
+  yellow: {
+    default: '#DBAC2C',
+    dark: '#C47F17',
+    light: '#EBE5F9',
+  },
+}
