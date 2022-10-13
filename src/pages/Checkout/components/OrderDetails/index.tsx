e5bc36bb7be3797f671e0c9e7c2defca9c442af0
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-10-13 14:41:49 -0300

    ✨ feat: creating total on order details

--- src/pages/Checkout/components/OrderDetails/index.tsx
+++ src/pages/Checkout/components/OrderDetails/index.tsx
@@ -1,15 +1,40 @@
 import { OrderItem } from './components/OrderItem'
-import { OrderDetailsBox, OrderDetailsContainer } from './styles'
+import {
+  OrderDetailsWrapper,
+  OrderDetailsContainer,
+  OrderTotalWrapper,
+  OrderTotalItems,
+  OrderTotalDelivered,
+  OrderTotalSum,
+  ConfirmButton,
+} from './styles'
 
 export function OrderDetails() {
   return (
     <OrderDetailsContainer>
       <h1>Cafés selecionados</h1>
 
-      <OrderDetailsBox>
+      <OrderDetailsWrapper>
         <OrderItem />
         <OrderItem />
-      </OrderDetailsBox>
+
+        <OrderTotalWrapper>
+          <OrderTotalItems>
+            <span>Total de itens</span>
+            <span>R$ 29,70</span>
+          </OrderTotalItems>
+          <OrderTotalDelivered>
+            <span>Entrega</span>
+            <span>R$ 3,50</span>
+          </OrderTotalDelivered>
+          <OrderTotalSum>
+            <span>Total</span>
+            <span>R$ 33,20</span>
+          </OrderTotalSum>
+        </OrderTotalWrapper>
+
+        <ConfirmButton>Confirmar pedido</ConfirmButton>
+      </OrderDetailsWrapper>
     </OrderDetailsContainer>
   )
 }
--- src/pages/Checkout/components/OrderDetails/styles.ts
+++ src/pages/Checkout/components/OrderDetails/styles.ts
@@ -12,11 +12,67 @@ export const OrderDetailsContainer = styled.section`
   }
 `
 
-export const OrderDetailsBox = styled.section`
+export const OrderDetailsWrapper = styled.section`
   background: ${({ theme }) => theme.card};
   padding: 2.5rem;
 
   border-radius: 6px;
   border-top-right-radius: 44px;
   border-bottom-left-radius: 44px;
+`
+
+export const OrderTotalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding-bottom: 1.5rem;
+`
+
+export const OrderTotal = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  margin-bottom: 0.75rem;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  span {
+    font-size: 0.875rem;
+    color: ${({ theme }) => theme.text};
+
+    &:last-child {
+      font-size: 1rem;
+    }
+  }
+`
+
+export const OrderTotalItems = styled(OrderTotal)``
+
+export const OrderTotalDelivered = styled(OrderTotal)``
+
+export const OrderTotalSum = styled(OrderTotal)`
+  span {
+    font-size: 1.25rem;
+    color: ${({ theme }) => theme.subtitle};
+    font-weight: bold;
+  }
+`
+
+export const ConfirmButton = styled.button`
+  width: 100%;
+  background: ${({ theme }) => theme.yellow.default};
+  border: none;
+  padding: 0.75rem;
+
+  color: ${({ theme }) => theme.white};
+  font-size: 0.875rem;
+  font-weight: bold;
+  text-transform: uppercase;
+
+  cursor: pointer;
 `
